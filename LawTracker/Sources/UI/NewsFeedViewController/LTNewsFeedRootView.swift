0044c66f39f36c4debd6be221065d3521a3c392e
Varvara Mironova <varvaramironova@MacBook-Air-Varvara.local>
2016-02-04 09:01:02 +0200

    NewsFeedViewController - created;

---
+++ LawTracker/Sources/UI/NewsFeedViewController/LTNewsFeedRootView.swift
@@ -0,0 +1,21 @@
+//
+//  LTNewsFeedRootView.swift
+//  LawTracker
+//
+//  Created by Varvara Mironova on 2/4/16.
+//  Copyright © 2016 VarvaraMironova. All rights reserved.
+//
+
+import UIKit
+
+class LTNewsFeedRootView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
